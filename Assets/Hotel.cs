4171c0ff53d2b18757a56fe6b8f5d960dc01714d
Finnard2112 <73263802+Finnard2112@users.noreply.github.com>
2025-04-12 12:27:48 -0400

    Saving progress

--- Assets/Hotel.cs
+++ Assets/Hotel.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 
-public class Stadium : Building
+public class Hotel : Building
 {
+    public static int hotelID = 1;
     public int base_price;
     private GameManager manager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        buildingName = "Capital Won Stadium";
-        buildingType = "Stadium";
+        buildingName = "Hotel " + hotelID;
+        hotelID = hotelID + 1;
+        buildingType = "Business";
         condition = Random.Range(1, 101);
         size = Random.Range(1, 4);
-        base_price = 5000000;
-        maintenanceFee = 5000;
-        attractiveness = 5;
-        condition_drop = 1.0f;
+        base_price = 200000;
+        maintenanceFee = 20;
+        attractiveness = 60;
+        condition_drop = 0.8f;
         manager = GameManager.instance;
     }
 
