748f9f32ff0b6076cff8ba95536e20462f0c8f43
abmanos <austinbmanos@gmail.com>
2025-04-12 12:36:16 -0400

    Base Map Updated

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -3,19 +3,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Camera cam;
+    public int speed = 1;
     void Update()
     {
         if(Input.GetKey(KeyCode.W)){
-            cam.transform.position = new Vector2(cam.transform.position.x, cam.transform.position.y + 0.002f);
+            cam.transform.position = new Vector2(cam.transform.position.x, cam.transform.position.y + (speed * Time.deltaTime));
         }
         if(Input.GetKey(KeyCode.S)){
-            cam.transform.position = new Vector2(cam.transform.position.x, cam.transform.position.y - 0.002f);
+            cam.transform.position = new Vector2(cam.transform.position.x, cam.transform.position.y - (speed * Time.deltaTime));
         }
         if(Input.GetKey(KeyCode.D)){
-            cam.transform.position = new Vector2(cam.transform.position.x + 0.002f, cam.transform.position.y);
+            cam.transform.position = new Vector2(cam.transform.position.x + (speed * Time.deltaTime), cam.transform.position.y);
         }
         if(Input.GetKey(KeyCode.A)){
-            cam.transform.position = new Vector2(cam.transform.position.x - 0.002f, cam.transform.position.y);
+            cam.transform.position = new Vector2(cam.transform.position.x - (speed * Time.deltaTime), cam.transform.position.y);
         }
     }
 }
